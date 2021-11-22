438e8364bc131db84dc277f3a426497cf95b5543
Samson Joseph <sjosep45@terpmail.umd.edu>
2021-11-22 15:39:18 -0500

    Worked on updateDynamoDB.py

--- updateDynamoDB_test.py
+++ updateDynamoDB_test.py
@@ -1,5 +1,7 @@
 import gspread
 import boto3
+from boto3.dynamodb.conditions import Key
+from boto3.dynamodb.conditions import Attr
 import pprint
 import time
 import json
@@ -15,9 +17,9 @@ num_of_cols = worksheet.col_count
 with open ('column_names.json', 'r') as f:
     col_names = json.load(f)
     
-for i in range(10):
-    if res[i]["Does your school have an active garden? (Check all that apply)"].find('Vegetable garden') == 0:
-        print(res[i]["Does your school have an active garden? (Check all that apply)"])
+# for i in range(10):
+#     if res[i]["Does your school have an active garden? (Check all that apply)"].find('Vegetable garden') == 0:
+#         print(res[i]["Does your school have an active garden? (Check all that apply)"])
         
 ACTIVE_GARDENS = [
     "Vegetable garden", "Native garden", "Butterfly garden", "Rain garden", 
@@ -38,7 +40,34 @@ COMPOSTING = [
      "We did not compost at our school", "Vermiculture", "Drum compost", 
      "Open frame", "Send Compost to Local Composting Facility/Farm", "I don't know" 
 ]
-        
+
+def getValuesForActiveGargen(googleValue, gardenType):
+    if googleValue.find(gardenType) != -1:
+        return "Yes"    
+    return "No"
+
+def getValuesForRecycle(googleValue, recycle):
+    if googleValue.find(recycle) != -1:
+        return '"Yes",'    
+    return '"No",'
+
+def getValuesForRecyclingProgram(googleValue, recyclingProgram):
+    if googleValue.find(recyclingProgram) != -1:
+        return '"Yes",'    
+    return '"No",'
+
+def getValuesForComposting(googleValue, composting):
+    if googleValue.find(composting) != -1:
+        return '"Yes",'    
+    return '"No",'
+
+# def getSchoolAdditionalInfo(schoolName):
+#     for schoolInfo in KNOWN_SCHOOLS_INFO:
+#         if schoolInfo[0].find(schoolName) != -1:
+#             return str(schoolInfo[1]) + "," + str(schoolInfo[2]) + ",\"" + str(schoolInfo[3]) + "\",\"" + str(schoolInfo[4]) + "\"," 
+
+#     return "38.7849,-76.8721,'Data Not Available','Data Not Available',"  
+## Default value if we don't find the school
 db_columnNames = [
     "section1_time_stamp",
     "section1_email",
@@ -133,22 +162,142 @@ db_columnNames = [
     "picture",
     "website"
 ]
-print(db_columnNames[0])
+# print(res[65])
+# for i in range(len(res)):
+#     print(i)
 
-# column_names= []
-# for col in range(num_of_cols):
-#     if col < 66:
-#         print(col)
-#         columns = worksheet.col_values(col+1)
-#         column_names.append(columns[0])
-#         # print(column_names[-1])
-#         time.sleep(1)
-# column_dict={"names":column_names}
-# with open('column_names.json','w') as f:
-#     json.dump(column_dict, f)
-# print(column_names)
 client = boto3.resource('dynamodb')
 table = client.Table('testdb')
+input = {"section1_time_stamp":"test",
+        "section1_email":"test",
+        "section1_school_name":"test",
+        "section1_green_school_certification":"test",
+        "section1_active_garden_vegetable_garden":"test",
+        "section1_active_garden_native_garden":"test",
+        "section1_active_garden_butterfly_garden":"test",
+        "section1_active_garden_rain_garden":"test",
+        "section1_active_garden_zen_garden":"test",
+        "section1_active_garden_herb_garden":"test",
+        "section1_active_garden_no_gardens_on_campus":"test",
+        "section1_active_garden_dont_know":"test",
+        "section1_recycle_at_breakfast":"test",
+        "section1_recycle_at_lunch":"test",
+        "section1_recycle_in_the_classroom":"test",
+        "section1_recycle_not_at_all":"test",
+        "section1_recycle_dont_know":"test",
+        "section1_recycling_program_ink_cartridge_recycling":"test",
+        "section1_recycling_program_phones_batteries_other":"test",
+        "section1_recycling_program_terra_cycling":"test",
+        "section1_recycling_program_color_cycle_crayola":"test",
+        "section1_recycling_program_pepsi_recycle_rally":"test",
+        "section1_recycling_program_none_programs_activities":"test",
+        "section1_recycling_program_dont_know":"test",
+        "section1_composting_we_did_not_compost_at_our_school":"test",
+        "section1_composting_vermiculture":"test",
+        "section1_composting_drum_compost":"test"
+        
+    }
+
+# for i in range(10):
+# for i in range(len(res)):
+#     input2 = {'pkey':i,
+#               'section1_time_stamp': res[i]['Timestamp'],
+#               "section1_email":res[i]['Email Address'],
+#               "section1_green_school_certification":res[i]['Does your school have a MD Green School Certification?'],
+#               "section1_active_garden_vegetable_garden":getValuesForActiveGargen(res[i]['Does your school have an active garden? (Check all that apply)'], "Vegetable garden")
+#               }
+#     table.put_item(Item=input2)
+
+# for i in range(10):
+q= table.scan(
+        # KeyConditionExpression = Key('pkey').eq(i) & Key('section1_green_school_certification')
+    FilterExpression = Attr('section1_green_school_certification').eq('No')
+)
+
+res_len =   len(q['Items'])
+for i in range(res_len):
+    # print(q[i])
+    print(q['Items'][i]['pkey'],q['Items'][i]['section1_green_school_certification'])
+    # print(q['Items'][i]['pkey'])
+
+# "section1_composting_open_frame",
+        # "section1_composting_send_compost_local_facility_farm",
+        # "section1_composting_dont_know",
+        # "section1_cleanup_volunteer_effort",
+        # "section1_waste_reduction_comments",
+        # "section2_reducing_water_strategy",
+        # "section2_stream",
+        # "section2_water_prevention_stream_bank_planting",
+        # "section2_water_prevention_erosion_control_project",
+        # "section2_water_prevention_painted_storm_drains",
+        # "section2_water_prevention_raingarden_bioretention_area_planted",
+        # "section2_water_prevention_no_mow_zone",
+        # "section2_water_prevention_rain_barrels",
+        # "section2_water_prevention_stream_cleaning",
+        # "section2_water_prevention_collected_litter",
+        # "section2_water_prevention_turf_eduction",
+        # "section2_water_prevention_surface_reduction",
+        # "section2_water_prevention_green_roof",
+        # "section2_water_prevention_retrofitted_sink_toilet_showers",
+        # "section2_runoff_strategy",
+        # "section2_water_conservation_comments",
+        # "section3_reduce_energy_strategy",
+        # "section3_energy_conservation_installed_efficient_lighting",
+        # "section3_energy_conservation_use_daylighting",
+        # "section3_energy_conservation_delamped",
+        # "section3_energy_conservation_planted_tree_shading",
+        # "section3_energy_conservation_use_of_blinds",
+        # "section3_renewable_energy",
+        # "section3_renewable_source_solar",
+        # "section3_renewable_source_wind",
+        # "section3_renewable_source_geothermal",
+        # "section3_energy_conservation_comments",
+        # "section4_restore_habitat",
+        # "section4_habitat_restoration_created_bird_houses",
+        # "section4_habitat_restoration_planted_native_trees",
+        # "section4_habitat_restoration_planted_native_shrubs",
+        # "section4_habitat_restoration_removal_invasive_species",
+        # "section4_habitat_restoration_created_native_habitat",
+        # "section4_habit_restoration_comments",
+        # "section4_enviro_learning_structures",
+        # "section4_env_learn_struct_interpretive_signage",
+        # "section4_env_learn_struct_trails_pathways",
+        # "section4_env_learn_struct_boardwalk_bridges",
+        # "section4_env_learn_struct_tree_plant_id_tags",
+        # "section4_env_learn_struct_outdoor_classroom",
+        # "section4_env_learn_struct_outdoor_environmental_art",
+        # "section4_env_learn_struct_greenhouse",
+        # "section4_env_learn_struct_tower_garden",
+        # "section4_env_learn_struct_weather_station",
+        # "section4_env_learn_struct_pond",
+        # "section4_env_learn_struct_hydroponics",
+        # "section4_env_learn_struct_aquaponics",
+        # "section4_enviro_structure_comments",
+        # "section5_no_idle_zone",
+        # "section5_formal_carpooling",
+        # "section5_electric_hybrid_parking",
+        # "section5_grow_donate_eat_garden",
+        # "section5_green_cleaning_products",
+        # "section5_community_science_program",
+        # "section6_enviro_awards",
+        # "section6_actions_not_mentioned",
+        # "latitude",
+        # "longitude",
+        # "picture",
+        # "website"
+
+
+
+
+
+
+
+
+
+
+
+
+
 # res = table.get_item(
 #     Key={
 #         'schoolNames':'Parkdale High School'
